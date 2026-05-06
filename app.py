5e66580f7f231691bf82a5c11b4bd9c30831d60b
arpithabeeresh701-jpg <arpithabeeresh701@gmail.com>
2026-05-06 02:16:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-import stream as st
+import streamlit as st
 import qrcode
-from PIL import image
-#page title
-st.title("qrcode generator")
-data =st.text_input("enter url")
- if st.button("generator qr"):
-    if data:
-        qr=qrcode.make(data)
-        qr.save("gr.png")
-        img=image.open("qr.png")
-        st.image(img,caption="generated qrcode")
-        with open ("qr.png","rb")as f:
-           st.dowload_button("Download QR",f,file_name="qr.png")
-   else:
-       st.warning("please enter some text")
+from PIL import Image
 
+# page title
+st.title("Qr code generator")
+data=st.text_input("enter url")
+if st.button("Generate QR"):
+    if data:
+      qr = qrcode.make(data)
+      qr.save("qr.png")
+
+      img = Image.open("qr.png")
+      st.image(img, caption="Generated QR Code")
+      
+      with open("qr.png","rb") as f:
+          st.download_button("Download QR", f, file_name="qr.png")
+else:
+   st.warning("Please enter some text")
